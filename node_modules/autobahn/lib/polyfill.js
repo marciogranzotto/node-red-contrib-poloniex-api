a65b8378df7da3ba1de104a568220ad0bdd040f9
Marcio <marcio@ckl.io>
2017-09-05 15:05:43 -0300

    Adds gitignore

--- node_modules/autobahn/lib/polyfill.js
+++
@@ -1,7 +0,0 @@
-require('./polyfill/object.js');
-require('./polyfill/array.js');
-require('./polyfill/string.js');
-require('./polyfill/function.js');
-require('./polyfill/console.js');
-require('./polyfill/typedarray.js');
-require('./polyfill/json.js');
